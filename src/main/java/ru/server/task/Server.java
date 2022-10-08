cefde01d92db9d62ed630b5d59a340c9bb43fa6e
Alexey Elagin <alexyel@mail.ru>
2022-10-08 17:07:05 +0300

    code in server reformated

--- src/main/java/ru/server/task/Server.java
+++ src/main/java/ru/server/task/Server.java
@@ -14,22 +14,20 @@ public class Server {
 
     public static void main(String[] args) {
 
-    try(ServerSocket serverSocket =new ServerSocket(PORT)) {
-        System.out.println("Server started...");
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("Server started...");
 
 
-        while (true) {
-            try (Socket clientSocket = serverSocket.accept();
-                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())))
-             {
-                 System.out.println(in.readLine());
-                 System.out.println("Входящее соединение с адреса:"+clientSocket.getInetAddress());
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+                    System.out.println(in.readLine());
+                    System.out.println("Входящее соединение с адреса:" + clientSocket.getInetAddress());
 
+                }
             }
-        }
-    }
-    catch(IOException e){
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
