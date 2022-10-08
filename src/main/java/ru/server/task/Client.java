bd7412b01a6cb47f9905a178875266345635df16
Alexey Elagin <alexyel@mail.ru>
2022-10-08 17:02:59 +0300

    project added

---
+++ src/main/java/ru/server/task/Client.java
@@ -0,0 +1,24 @@
+package ru.server.task;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+    private static final int PORT = 8081;
+    private static final String HOST = "localhost";
+
+    public static void main(String[] args) {
+        try (Socket clientSocket = new Socket(HOST, PORT);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            out.println("Hello, it's me...");
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/ru/server/task/Server.java
@@ -0,0 +1,36 @@
+package ru.server.task;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static java.lang.System.in;
+
+public class Server {
+    private static final int PORT = 8081;
+
+    public static void main(String[] args) {
+
+    try(ServerSocket serverSocket =new ServerSocket(PORT)) {
+        System.out.println("Server started...");
+
+
+        while (true) {
+            try (Socket clientSocket = serverSocket.accept();
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())))
+             {
+                 System.out.println(in.readLine());
+                 System.out.println("Входящее соединение с адреса:"+clientSocket.getInetAddress());
+
+            }
+        }
+    }
+    catch(IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
